c76eb40e3edc6828d272cefc5f52df0a22a3e6c0
Igor Deon <igordeon709@gmail.com>
2023-02-26 04:31:55 -0300

    City and State database auto insertion

--- src/main/java/com/cursoSpring/projeto/ProjetoApplication.java
+++ src/main/java/com/cursoSpring/projeto/ProjetoApplication.java
@@ -1,9 +1,13 @@
 package com.cursoSpring.projeto;
 
 import com.cursoSpring.projeto.domain.Category;
+import com.cursoSpring.projeto.domain.City;
 import com.cursoSpring.projeto.domain.Product;
+import com.cursoSpring.projeto.domain.State;
 import com.cursoSpring.projeto.repositories.CategoryRepository;
+import com.cursoSpring.projeto.repositories.CityRepository;
 import com.cursoSpring.projeto.repositories.ProductRepository;
+import com.cursoSpring.projeto.repositories.StateRepository;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -18,11 +22,19 @@ public class ProjetoApplication implements CommandLineRunner {
   @Autowired
   private ProductRepository productRepository;
 
+  @Autowired
+  private StateRepository stateRepository;
+
+  @Autowired
+  private CityRepository cityRepository;
+
   public static void main(String[] args) {
     SpringApplication.run(ProjetoApplication.class, args);
   }
 
   public void run(String... args) throws Exception {
+    // Relacoes de produtos e categorias
+
     Product prod1 = new Product(null, "Computador", 2200.00);
     Product prod2 = new Product(null, "Impressora", 800.00);
     Product prod3 = new Product(null, "Mouse", 80.00);
@@ -39,5 +51,20 @@ public class ProjetoApplication implements CommandLineRunner {
 
     categoryRepository.saveAll(Arrays.asList(cat1, cat2));
     productRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
+
+    // Relacoes de cidades e estados
+
+    State state1 = new State(null, "Minas Gerais");
+    State state2 = new State(null, "São Paulo");
+
+    City city1 = new City(null, "Uberlândia", state1);
+    City city2 = new City(null, "Campinas", state2);
+    City city3 = new City(null, "Ubatuba", state2);
+
+    state1.getCities().addAll(Arrays.asList(city1));
+    state2.getCities().addAll(Arrays.asList(city2, city3));
+
+    stateRepository.saveAll(Arrays.asList(state1, state2));
+    cityRepository.saveAll(Arrays.asList(city1, city2, city3));
   }
 }
